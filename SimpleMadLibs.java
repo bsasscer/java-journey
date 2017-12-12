996a990d22238e3bcf1dafb21355c07e16104eef
bsasscer <smithbi.maricela@gmail.com>
2017-12-12 14:37:55 -0800

    hard code conditional if statement to exit our program

--- SimpleMadLibs.java
+++ SimpleMadLibs.java
@@ -5,6 +5,11 @@ public class SimpleMadLibs {
     public static void main(String[] args) {
         //__Name__ is a __adjective__ __noun__. She is always __adverb__ __verb__.
         Console console = System.console();
+        int age = 12;
+        if (age < 13) {
+            console.printf("You need to be 13 years old to access this program.\n");
+            System.exit(0);
+        }
         String name = console.readLine("Enter your name: ");
         String adjective = console.readLine("Enter an adjective: ");
         String noun = console.readLine("Enter a noun: ");
