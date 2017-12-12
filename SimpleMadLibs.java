f0c7de903780370859b9bc6eb355035753c004a9
bsasscer <smithbi.maricela@gmail.com>
2017-12-12 14:05:39 -0800

    add more parts of speech to madlibs

--- SimpleMadLibs.java
+++ SimpleMadLibs.java
@@ -3,9 +3,16 @@ import java.io.Console;
 public class SimpleMadLibs {
 
     public static void main(String[] args) {
+        //__Name__ is a __adjective__ __noun__. She is always __adverb__ __verb__.
         Console console = System.console();
         String name = console.readLine("Enter your name: ");
         String adjective = console.readLine("Enter an adjective: ");
-        console.printf("%s is very %s ", name, adjective);
+        String noun = console.readLine("Enter a noun: ");
+        String adverb = console.readLine("Enter an adverb: ");
+        String verb = console.readLine("Enter a verb ending in -ing: ");
+
+        console.printf("Your SimpleMadLibs: \n--------------\n");
+        console.printf("%s is a very %s %s. ", name, adjective, noun);
+        console.printf("They are always %s %s. \n", adverb, verb);
     }
 }
