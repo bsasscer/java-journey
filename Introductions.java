2330cd1995adb374fadc92156ab50ed30fa01606
bsasscer <smithbi.maricela@gmail.com>
2017-12-13 15:22:53 -0800

    add data structures practice

--- Introductions.java
+++ Introductions.java
@@ -8,5 +8,5 @@ public class Introductions {
         String firstName = console.readLine("What is your name? ");
         console.printf("Hello, my name is %s\n", firstName);
         console.printf("%s is learning how to write Java\n", firstName);
-  }
+    }
 }
