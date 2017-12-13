2e74b8f17f46e8c520cfee68bec84f9c567ac43f
bsasscer <smithbi.maricela@gmail.com>
2017-12-13 10:41:16 -0800

    add simple car loan calculator

---
+++ conditionals/CarLoan.java
@@ -0,0 +1,32 @@
+/*
+Simple car loan payment calculator. Calculates the monthly car payment a user should expect to pay after taking out a loan.
+	Includes:
+  -- Car loan amount
+  -- Interest rate of the loan
+  -- Length of the loan, in years
+  -- Down payment
+*/
+
+public class CarLoan {
+    public static void main(String[] args) {
+        int carLoan = 10000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
+
+        if (loanLength <= 0 || interestRate <0) {
+            System.out.println("Error! You must take out a valid car loan");
+        }
+        else if (downPayment >= carLoan) {
+            System.out.println("The car can be paid in full.");
+        }
+        else {
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalance = remainingBalance / months;
+            int interest = monthlyBalance * interestRate / 100;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.println(monthlyPayment);
+        }
+	}
+}
