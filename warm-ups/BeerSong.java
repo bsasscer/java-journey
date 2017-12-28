25d0e48a0defd435c4afe1a5f3668c5f15b01437
bsasscer <smithbi.maricela@gmail.com>
2017-12-28 10:06:41 -0800

    fix tabbing for beersong and create a program that randomly picks words from three lists of words, then prints out the result

--- warm-ups/BeerSong.java
+++ warm-ups/BeerSong.java
@@ -8,11 +8,11 @@ public class BeerSong {
             if (beerNum == 1) {
                 word = "bottle"; // singular, as in ONE bottle.
             }
-                System.out.println(beerNum + " " + word + " of beer on the wall.");
-                System.out.println(beerNum + " " + word + " of beer.");
-                System.out.println("Take one down.");
-                System.out.println("Pass it around.");
-                beerNum = beerNum - 1;
+            System.out.println(beerNum + " " + word + " of beer on the wall.");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum - 1;
         }
         System.out.println("No more bottles of beer on the wall.");
     }
