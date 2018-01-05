6dac20d5732f43232216c68049d5a646b6b0dadd
bsasscer <smithbi.maricela@gmail.com>
2018-01-05 09:44:02 -0800

    Polymorphism challenge 1.5

---
+++ warm-ups/Student.java
@@ -0,0 +1,15 @@
+public class Student extends Person
+{
+    public Student(String name)  {
+        super(name);
+    }
+
+    public boolean isAsleep( int hr ) // override
+    { return 2 < hr && 8 > hr; }
+
+    public static void main(String[] args) {
+        Person p;
+        p = new Student("Sally");
+        p.status(1); //we're calling .status on a student object
+    }
+}
