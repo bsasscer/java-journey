92ba35ae0b04213ac2ba0659763f6241fe5deda3
bsasscer <smithbi.maricela@gmail.com>
2018-02-05 10:22:42 -0800

    Create pezdispenser class modeled after a real life pezdispenser

---
+++ oo-java/PezBuilder.java
@@ -0,0 +1,25 @@
+public class PezBuilder {
+
+    public static void main(String[] args) {
+        System.out.println("We are making a new PEZ Dispenser");
+        PezDispenser dispenser = new PezDispenser("Chewbaca");
+        System.out.printf("The dispenser is %s %n",
+                            dispenser.getCharacterName()
+                        );
+        if (dispenser.isEmpty()) {
+            System.out.println("Dispenser is empty");
+        }
+        System.out.println("Filling the dispenser with PEZ...");
+        dispenser.fill();
+        if (!dispenser.isEmpty()) {
+            System.out.println("Dispenser is full");
+        }
+
+        while (dispenser.dispense()) {
+            System.out.println("Chomp!");
+        }
+        if (dispenser.isEmpty()) {
+            System.out.println("Ate all the PEZ");
+        }
+    }
+}
---
+++ oo-java/PezDispenser.java
@@ -0,0 +1,31 @@
+class PezDispenser {
+    public static final int MAX_PEZ = 12;
+    final private String characterName;
+    private int pezCount;
+
+    public PezDispenser(String characterName) {
+        this.characterName = characterName;
+        pezCount = 0;
+    }
+
+    public void fill() {
+        pezCount = MAX_PEZ;
+    }
+
+    public boolean dispense() {
+        boolean wasDispensed = false;
+        if (!isEmpty()) {
+            pezCount--;
+            wasDispensed = true;
+        }
+        return wasDispensed;
+    }
+
+    public boolean isEmpty() {
+        return pezCount == 0;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+}
