d37797be5b667c6854bbdbd60b91e2b28b3b6ec7
bsasscer <smithbi.maricela@gmail.com>
2018-02-07 11:19:34 -0800

    add method that informs the user about the current state of the hangman puzzle

--- oo-java/Hangman.java
+++ oo-java/Hangman.java
@@ -3,11 +3,13 @@ public class Hangman {
     public static void main(String[] args) {
         Game game = new Game("curmudgeon");
         Prompter prompter = new Prompter(game);
+        prompter.displayProgress();
         boolean isHit = prompter.promptForGuess();
         if (isHit) {
             System.out.println("We got a hit!");
         } else {
             System.out.println("Miss!");
         }
+        prompter.displayProgress();
     }
 }
